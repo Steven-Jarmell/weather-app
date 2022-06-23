1fa9bf042b4d122a6bf4699dee366abc3fe1e972
Steven Jarmell <sjj27@pitt.edu>
2022-06-23 12:04:58 -0400

    Finished simple weather app

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const searchItem = document.getElementById('search-bar');
 const submitBtn = document.querySelector('.submit-btn');
 const mode = document.querySelector('.toggle-input');
 let dataType = false; // True -> Fahrenheit ; False -> Celsius
+let submitted = false;
 
 searchForm.addEventListener('submit', (e)=> {
     e.preventDefault();
@@ -15,34 +16,41 @@ const feelsLike = document.querySelector('.feelsLike');
 
 submitBtn.addEventListener('click', async (e) => {
     if (searchItem.value === "") return;
+
     const data = await getData(searchItem.value);
+
+    submitted = true;
+    const displayContent = document.querySelector('.search-result');
+    displayContent.classList.add('visible');
 
     const cityName = document.querySelector('.cityName');
     cityName.innerText = data.cityName;
 
     if (dataType) {
-        temperature.innerText = convertToFahrenheit(data.temperature);
-        feelsLike.innerText = convertToFahrenheit(data.feelsLike);
+        temperature.innerText = 'Temperature: ' + convertToFahrenheit(data.temperature);
+        feelsLike.innerText = 'Feels Like: ' + convertToFahrenheit(data.feelsLike);
     }
     else {
-        temperature.innerText = Math.round(data.temperature);
-        feelsLike.innerText = Math.round(data.feelsLike);
+        temperature.innerText = 'Temperature: ' + Math.round(data.temperature);
+        feelsLike.innerText = 'Feels Like: ' + Math.round(data.feelsLike);
     }
 
     const humidity = document.querySelector('.humidity');
-    humidity.innerText = data.humidity;
+    humidity.innerText = 'Humidity: ' + data.humidity + '%';
 });
 
 mode.addEventListener('click', (e) => {
     dataType = !dataType;
 
+    if (searchItem.value === "" || !submitted) return;
+
     if (dataType) {
-        temperature.innerText = convertToFahrenheit(parseInt(temperature.innerText));
-        feelsLike.innerText = convertToFahrenheit(parseInt(feelsLike.innerText));
+        temperature.innerText = 'Temperature: ' + convertToFahrenheit(parseInt(temperature.innerText.split(' ')[1]));
+        feelsLike.innerText = 'Feels Like: ' + convertToFahrenheit(parseInt(feelsLike.innerText.split(' ')[2]));
     }
     else {
-        temperature.innerText = convertToCelsius(parseInt(temperature.innerText));
-        feelsLike.innerText = convertToCelsius(parseInt(feelsLike.innerText));
+        temperature.innerText = 'Temperature: ' + convertToCelsius(parseInt(temperature.innerText.split(' ')[1]));
+        feelsLike.innerText = 'Feels Like: ' + convertToCelsius(parseInt(feelsLike.innerText.split(' ')[2]));
     }
 });
 
